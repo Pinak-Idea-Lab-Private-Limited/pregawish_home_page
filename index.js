04218fe85fd11033d1c8291cae10630b12bdd184
shaikat17 <shaikatpal56@gmail.com>
2023-09-19 11:34:55 +0600

    Dial code number added.

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const input = document.querySelector("#number");
 
  intlTelInput(input, {
     initialCountry: 'in',
+    separateDialCode: true,
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
   });
