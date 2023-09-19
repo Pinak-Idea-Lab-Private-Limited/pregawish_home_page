a13d116080ed3e9521878188fb90c0cd7ad81c7d
shaikat17 <shaikatpal56@gmail.com>
2023-09-19 11:29:31 +0600

    Book Appoinment section initial done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 const input = document.querySelector("#number");
 
  intlTelInput(input, {
-    initialCountry: geoplugin_countryCode() || 'in',
+    initialCountry: 'in',
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
   });
